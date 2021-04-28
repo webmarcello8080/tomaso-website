a3ccd3769b28b40da1a376be69e337c74dd69309
Marcello Perri <webmarcello8080@gmail.com>
2021-04-28 12:07:53 +0100

    product archive page and product design in archive page done

--- web/app/themes/tomaso-theme/classes/frontend/Buttons.class.php
+++ web/app/themes/tomaso-theme/classes/frontend/Buttons.class.php
@@ -4,7 +4,7 @@ namespace NewWebMarcello\Frontend;
 
 class Buttons{
 
-   public function get_login($text, $target = '_self', $classes = ''){
+   public function get_login( $text, $target = '_self', $classes = '' ){
       ?>
       <a class="<?= $classes ?>" 
          href="<?= esc_url( wp_login_url( get_permalink() ) ); ?>" 
@@ -13,5 +13,16 @@ class Buttons{
          <?php _e( $text, 'tomaso_azara' ); ?>
       </a>
       <?php
+   }
+
+   public function get_add_to_card( $product_id, $classes = '', $text = 'Add to Card' ){
+      $product = wc_get_product( $product_id );
+      // get the "Checkout Page" URL
+      $cart_url = WC()->cart->get_cart_url();
+   
+      // run only on simple products
+      if( $product->is_type( 'simple' ) ){
+         echo '<a href="' . $cart_url . '?add-to-cart=' . $product_id . '" class="' . $classes . '">' . $text . '</a>';
+      }
    }
 }
--- web/app/themes/tomaso-theme/template-parts/loop/product.php
+++ web/app/themes/tomaso-theme/template-parts/loop/product.php
@@ -1,3 +1,7 @@
+<?php
+$buttons = new \NewWebMarcello\Frontend\Buttons;
+$_product = wc_get_product( get_the_ID() );
+?>
 <div class="col-md-4">
    <div class="card border-0 rounded-lg my-3 text-left single-product-block">
       <?php if(has_post_thumbnail()): ?>
@@ -8,9 +12,12 @@
       <div class="card-body product-body">
          <div class="product-header pb-0 d-flex justify-content-between">
             <a href="<?php the_permalink() ?>" ><h6 class="product-title"><?php the_title(); ?></h6></a>
-            <h6 class="product-price text-secondary">£<?= get_post_meta( get_the_ID(), 'product_option_price', true ); ?> Kg</h6>
+            <h6 class="product-price text-secondary">£<?= $_product->get_price(); ?> Kg</h6>
          </div>
          <p class="card-text product-descrition"><small><?= get_the_excerpt() ?></small></p>
+         <div class="product-button text-center mt-4">
+            <?php $buttons->get_add_to_card( get_the_ID(), 'btn btn-sm btn-secondary', 'Add to Card' ) ?>
+         </div>
       </div>
    </div>
 </div>

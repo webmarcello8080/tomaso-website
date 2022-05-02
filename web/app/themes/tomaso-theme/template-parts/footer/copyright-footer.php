0b849c0ae26e186f366b384afc9521ffff55a4c6
Marcello Perri <webmarcello8080@gmail.com>
2022-05-02 13:32:27 +0100

    header and footer and logos

--- web/app/themes/tomaso-theme/template-parts/footer/copyright-footer.php
+++ web/app/themes/tomaso-theme/template-parts/footer/copyright-footer.php
@@ -2,6 +2,6 @@
    <?php dynamic_sidebar('copyright_area'); ?>
    <div class="footer-footer p-2 text-white">
       <small>· Designed by <a href="http://webmarcello.co.uk" target="_black">WMP</a> ·</small>
-      <small>SARDINIA MICRO CONTINENT LTD - Company number 13330277</small>
+      <small>Sardinia Micro Continent LTD - Company number 13330277</small>
    </div>
 </section>

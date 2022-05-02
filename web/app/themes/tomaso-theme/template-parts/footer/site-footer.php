591475e3a3673f9b5176b46ed417f180efc30c41
Marcello Perri <webmarcello8080@gmail.com>
2022-05-02 11:54:06 +0100

    header and footer renew

--- web/app/themes/tomaso-theme/template-parts/footer/site-footer.php
+++ web/app/themes/tomaso-theme/template-parts/footer/site-footer.php
@@ -1,17 +1,26 @@
 <?php
 // get background images
-$dots = file_get_contents( get_template_directory() . "/dist/images/background/small-background-dots.svg");
+$dots = file_get_contents(get_template_directory() . "/dist/images/background/small-background-dots.svg");
+$facebook = file_get_contents(get_template_directory() . "/dist/images/logos/facebook.svg");
+$instagram = file_get_contents(get_template_directory() . "/dist/images/logos/instagram.svg");
 ?>
 <footer id="footer" class="footer-section bg-light row py-4">
    <div class="small-container footer-wrapper">
       <div class="footer-area footer-area-one-wrapper my-5">
-         <?php dynamic_sidebar( 'footer_area_one' ); ?>
+         <?php dynamic_sidebar('footer_area_one'); ?>
       </div>
       <div class="footer-area footer-area-two-wrapper my-5">
-         <?php dynamic_sidebar( 'footer_area_two' ); ?>
+         <?php dynamic_sidebar('footer_area_two'); ?>
       </div>
       <div class="footer-area footer-area-three-wrapper my-5">
-         <?php dynamic_sidebar( 'footer_area_three' ); ?>
+         <?php dynamic_sidebar('footer_area_three'); ?>
+      </div>
+      <div class="footer-area my-5 d-flex d-flex justify-content-center align-items-center follow-us-wrapper">
+         <p class="h5 text-primary mx-3 mb-0">Follow Us:</p>
+         <div>
+            <a class="follow-us-logo" href=""><?= $facebook ?></a>
+            <a class="follow-us-logo" href=""><?= $instagram ?></a>
+         </div>
       </div>
    </div>
    <div class="background-dots"><?= $dots ?></div>
--- web/app/themes/tomaso-theme/template-parts/header/site-header.php
+++ web/app/themes/tomaso-theme/template-parts/header/site-header.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
 $header = new \NewWebMarcello\frontend\Header();
 ?>
 <header class="site-header">
    <nav class="navbar navbar-expand-lg navbar-wrapper small-container">
-      <?php $header->get_logo('navbar-brand text-center logo-wrapper','img-fluid logo-img') ?>
-
-      <?php get_template_part( 'template-parts/menu/site-menu' ); ?>
+      <a href="<?= esc_url(home_url('/'));  ?>" class="navbar-brand text-center logo-wrapper" rel="home">
+         <h3 class="mb-0 site-description"><?= get_bloginfo('description')  ?></h3>
+      </a>
+      <?php get_template_part('template-parts/menu/site-menu'); ?>
    </nav>
 </header>
+<h1 class="site-logo text-center"><?= get_bloginfo('name') ?></h1>

d9f7f71a1e26b59be76929be78368198394cd314
Marcello Perri <webmarcello8080@gmail.com>
2022-05-02 12:50:50 +0100

    header and footer and logos

--- web/app/themes/tomaso-theme/blocks/block-slogan.php
+++ web/app/themes/tomaso-theme/blocks/block-slogan.php
@@ -1,8 +1,8 @@
-<section class="slogan-block my-4 text-center d-flex justify-content-center <?php block_field('className'); ?>" >
+<section class="slogan-block my-4 text-center d-flex justify-content-center <?php block_field('className'); ?>">
    <h1 class="slogan-block-first">
       <?php block_field('slogan-one-text'); ?>
    </h1>
-   <h1 class="slogan-block-second">
-      <?php block_field('slogan-two-text'); ?>
-   </h1>
+   <div class="slogan-block-second">
+      <img src="<?php block_field('slogan-image') ?>" alt="">
+   </div>
 </section>
--- web/app/themes/tomaso-theme/template-parts/header/site-header.php
+++ web/app/themes/tomaso-theme/template-parts/header/site-header.php
@@ -3,10 +3,13 @@ $header = new \NewWebMarcello\frontend\Header();
 ?>
 <header class="site-header">
    <nav class="navbar navbar-expand-lg navbar-wrapper small-container">
-      <a href="<?= esc_url(home_url('/'));  ?>" class="navbar-brand text-center logo-wrapper" rel="home">
+      <a href="<?= esc_url(home_url('/'));  ?>" class="navbar-brand text-center logo-wrapper d-flex align-items-center" rel="home">
          <h3 class="mb-0 site-description"><?= get_bloginfo('description')  ?></h3>
       </a>
       <?php get_template_part('template-parts/menu/site-menu'); ?>
    </nav>
 </header>
-<h1 class="site-logo text-center mt-2"><?= get_bloginfo('name') ?></h1>
+<div class="d-flex align-items-center justify-content-center site-logo-wrapper">
+   <h1 class="site-logo text-center"><?= get_bloginfo('name') ?></h1>
+   <img class="site-logo-img mr-3" src="<?= get_template_directory_uri() . '/dist/images/logos/smc-logo.jpg' ?>" alt="">
+</div>

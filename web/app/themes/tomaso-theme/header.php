7c2715fff8dfa72f14df71f9103b4c91a225df16
Marcello Perri <webmarcello8080@gmail.com>
2022-05-02 13:25:39 +0100

    header and footer and logos

--- web/app/themes/tomaso-theme/header.php
+++ web/app/themes/tomaso-theme/header.php
@@ -3,11 +3,27 @@
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+
 <head>
    <?php wp_head(); ?>
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-480HZ29BHM"></script>
+   <script>
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+         dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'G-480HZ29BHM');
+   </script>
 </head>
+
 <body <?php body_class(); ?>>
-<div id="page" class="site container-xl">
-   <?php get_template_part( 'template-parts/header/site-header' ); ?>
-   <main id="main">
+   <div id="page" class="site container-xl">
+      <?php get_template_part('template-parts/header/site-header'); ?>
+      <main id="main">
